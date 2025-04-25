aba0f75b5e93cdf52b33f4bd0a1caab8c8b26352
jakeyvee <jakeyveee@gmail.com>
2025-04-26 00:05:49 +0800

    add lut parser

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -202,3 +202,25 @@ export const setupAddressLookupTable = async (
   }
   return lut;
 };
+
+export const getAddressLookupTableAccounts = async (
+  keys: string[],
+  connection: Connection
+): Promise<AddressLookupTableAccount[]> => {
+  const addressLookupTableAccountInfos =
+    await connection.getMultipleAccountsInfo(
+      keys.map((key) => new PublicKey(key))
+    );
+
+  return addressLookupTableAccountInfos.reduce((acc, accountInfo, index) => {
+    const addressLookupTableAddress = keys[index];
+    if (accountInfo) {
+      const addressLookupTableAccount = new AddressLookupTableAccount({
+        key: new PublicKey(addressLookupTableAddress),
+        state: AddressLookupTableAccount.deserialize(accountInfo.data),
+      });
+      acc.push(addressLookupTableAccount);
+    }
+    return acc;
+  }, new Array<AddressLookupTableAccount>());
+};

6238a09f0efb4547054151e8546b47ba3ed9fc92
jakeyvee <jakeyveee@gmail.com>
2025-04-25 20:58:44 +0800

    add cancelRequestWithdrawVault

---
+++ src/scripts/user-cancel-request-withdraw-vault.ts
@@ -0,0 +1,41 @@
+// NOTE: WILL ONLY SUCCEED IF THERE IS A REQUEST WITHDRAWAL OUTSTANDING
+import {
+  Connection,
+  Keypair,
+  PublicKey,
+  TransactionInstruction,
+} from "@solana/web3.js";
+import * as fs from "fs";
+import { sendAndConfirmOptimisedTx } from "../utils/helper";
+import { VoltrClient } from "@voltr/vault-sdk";
+import { vaultAddress } from "../../config/base";
+
+const userKpFile = fs.readFileSync(process.env.USER_KP_FILE!, "utf-8");
+const userKpData = JSON.parse(userKpFile);
+const userSecret = Uint8Array.from(userKpData);
+const userKp = Keypair.fromSecretKey(userSecret);
+const user = userKp.publicKey;
+
+const vault = new PublicKey(vaultAddress);
+
+const connection = new Connection(process.env.HELIUS_RPC_URL!);
+const vc = new VoltrClient(connection);
+
+const cancelRequestWithdrawVaultHandler = async () => {
+  const ixs: TransactionInstruction[] = [];
+  const cancelRequestWithdrawVaultIx =
+    await vc.createCancelRequestWithdrawVaultIx({
+      userTransferAuthority: user,
+      vault,
+    });
+  ixs.push(cancelRequestWithdrawVaultIx);
+
+  const txSig = await sendAndConfirmOptimisedTx(
+    ixs,
+    process.env.HELIUS_RPC_URL!,
+    userKp
+  );
+  console.log("Cancel Request Withdraw Vault Tx Sig: ", txSig);
+};
+
+cancelRequestWithdrawVaultHandler();
